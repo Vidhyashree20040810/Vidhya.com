792b7d04a8279103d72bef7d831032bb51c99ea5
Vidhyashree20040810 <ksvidhyashree594@gmail.com>
2025-05-11 12:43:00 +0530

    Add files via upload

---
+++ 3Dvisualization.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const ThreeDVisualization = () => {
+  return (
+    <div>
+      <h2>3D Visualization</h2>
+      <p>See your space in stunning 3D.</p>
+    </div>
+  );
+};
+
+export default ThreeDVisualization;
